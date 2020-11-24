1e70c442025658da320d0ff8fb911291f3b8bde2
karapeoples <kararpeoples@yahoo.com>
2020-11-23 18:41:23 -0600

    Style Underline Again

--- src/components/forms/userforms/ClerkRegister.js
+++ src/components/forms/userforms/ClerkRegister.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
-import {useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { registerClerk } from '../../../redux/actions'
-import { Label, Input, Button, Spinner } from 'reactstrap'
+import { Label, Input, Button } from 'reactstrap'
 
 const ClerkRegister = () => {
-	const success = useSelector((state) => state.addSuccess)
   const dispatch = useDispatch()
 	const [clerk, setClerk] = useState({
 		fullName: '',
